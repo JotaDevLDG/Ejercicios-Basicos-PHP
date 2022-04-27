29a99daa64a32d3d2cfed921ea7cb3ab2172d89d
Juan José Iraita Cavero <85316826+SeiyaTw30@users.noreply.github.com>
2022-04-27 01:51:42 -0500

    ejercicio de suma en PHP
    suma en php en distintos metodos

---
+++ suma.php
@@ -0,0 +1,12 @@
+<?php
+//variables en PHP
+$numero1 = 2;
+$numero2 = 3;
+$res     = $numero1 + $numero2 ;
+
+//hecho con funcion
+function sumar($numero1,$numero2){
+  $res     = $numero1 + $numero2 ;
+  return $res;
+}
+?>
