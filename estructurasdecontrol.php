688ace9277b7967d19cda7395ed3bca73effdc9d
Juan José Iraita Cavero <85316826+SeiyaTw30@users.noreply.github.com>
2022-05-03 01:02:16 -0500

    Update V2.5

--- estructurasdecontrol.php
+++ estructurasdecontrol.php
@@ -14,7 +14,7 @@ if ($a > $b) {
   echo "a NO es mayor que b";
 }
 
-/* ejemplo 1 */
+/* estructura while  primera forma */
 
 $i = 1;
 while ($i <= 10) {
@@ -22,4 +22,49 @@ while ($i <= 10) {
                    $i antes del incremento
                    (post-incremento) */
 }
+//estructura while 2 forma 
+$i = 1;
+while ($i <= 10):
+    echo $i;
+    $i++;
+endwhile;
+
+// hacer mientras do - while 
+$i = 0;
+do {
+    echo $i;
+} while ($i > 0);
+
+// formas del PARA 
+
+/* ejemplo 1 */
+
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+}
+
+/* ejemplo 2 */
+
+for ($i = 1; ; $i++) {
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+}
+
+/* ejemplo 3 */
+
+$i = 1;
+for (; ; ) {
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+    $i++;
+}
+
+/* ejemplo 4 */
+
+for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
+
 ?>
